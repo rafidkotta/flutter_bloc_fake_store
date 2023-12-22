481ae820c910a25dc94ab55ec3522760f417bf9b
Rafid Kotta <getrafid@gmail.com>
2023-12-22 14:53:44 +0400

    floating action button build bug fixed

--- lib/home.dart
+++ lib/home.dart
@@ -30,39 +30,38 @@ class _StoreHomeState extends State<StoreHome> {
             SizedBox(width: 50,),
             Icon(Icons.shopping_basket),
             SizedBox(width: 8,),
-            Text("Fake store",style: TextStyle(fontWeight: FontWeight.bold),),
+            Text("Fake store", style: TextStyle(fontWeight: FontWeight.bold),),
           ],
         ),
         actions: const [
           ThemeSwitch(),
         ],
       ),
-      floatingActionButton: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          FloatingActionButton(
-            onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => BlocProvider.value(value: BlocProvider.of<StoreBloc>(context), child: const CartScreen())),);
-            },
-            child: const Icon(Icons.shopping_bag,color: Colors.white,),
-          ),
-          BlocBuilder<StoreBloc, StoreState>(
-            builder: (context, state) {
-              if(state.cart.isEmpty){
-                return Container();
-              }
-              return Positioned(
-                right: -4,
-                bottom: 40,
-                child: CircleAvatar(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: BlocBuilder<StoreBloc, StoreState>(
+        builder: (context, state) {
+          return Stack(
+            clipBehavior: Clip.none,
+            children: [
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => BlocProvider.value(value: BlocProvider.of<StoreBloc>(context), child: const CartScreen())),);
+                },
+                child: const Icon(Icons.shopping_bag, color: Colors.white,),
+              ),
+              if (state.cart.isNotEmpty)
+                Positioned(
+                  right: -4,
+                  bottom: 40,
+                  child: CircleAvatar(
                   radius: 12,
                   backgroundColor: Colors.green,
                   child: Text("${state.cart.length}"),
                 ),
-              );
-            },
-          ),
-        ],
+              )
+            ],
+          );
+        },
       ),
       body: const StoreScreen(), // This trailing comma makes auto-formatting nicer for build methods.
     );
